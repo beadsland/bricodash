5b79fa622540c47fbc1bab74324b60cce4333a8f
beadsland <beads.d.land@gmail.com>
2018-09-09 01:29:45 -0400

    canonical url; cleanup

--- do_dash.py
+++ do_dash.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from subprocess import call
 import time
@@ -10,17 +10,8 @@ cast = next(cc for cc in chromecasts if cc.device.friendly_name == "Hackerspace"
 
 cast.wait()
 
-print cast.status
-
+path = "http://hackmanhattan.duckdns.org:8888/hm/dash.php";
+patharr = "/usr/local/bin/catt cast_site".split()
+patharr.append(path)
 if cast.status.display_name == "Backdrop":
-  call("/usr/local/bin/catt cast_site http://192.168.43.191/hm/dash.php".split())
-
-
-## Get's stuck on "loading" screen unless forced
-#  d = dashcast.DashCastController()
-#  cast.register_handler(d)
-
-#  warning_message = "hello world!"
-#  d.load_url('http://hackmanhattan.duckdns.org:8080/hm/camera.php')
-#  time.sleep(30)
-
+  call(patharr)
