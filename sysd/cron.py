defe15822d84f898631795924856c5c9980092d5
beadsland <beads.d.land@gmail.com>
2018-09-10 02:23:39 -0400

    umbrella cron

---
+++ sysd/cron.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# This will likely run as a systemd service in the long run, but
+# for ease of development we'll do it as a cron job for now.
+#
+# This is an umbrella script that runs every minute and triggers
+# each of the underlying pull services for the dashboard. Primarily,
+# this is to limit cron clutter, but also, to avoid errors with
+# cron syntax.
+#
+# crontab user: hm
+
+import datetime
+import os
+import sys
+
+min = datetime.datetime.now().minute
+pwd = os.path.dirname(sys.argv[0])
+
+if 1:               os.system(pwd + "/../slack.py")
+if min % 15 == 0:   os.system(pwd + "/../meetup.py")
