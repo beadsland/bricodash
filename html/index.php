a8e43acaf6346e6e368912b3ae73ebb9bca82957
beadsland <beads.d.land@gmail.com>
2018-09-11 11:14:00 -0400

    rearrange elements

--- html/index.php
+++ html/index.php
@@ -15,48 +15,40 @@
 </head>
 <body>
 
-  <div style="width:35%;z-index:1;position:absolute;bottom:0;right:0;margin-top:0"
-      id="slack-chat"></div>
+  <div style="width: 100%; z-index: 10; position:absolute; margin-bottom: 30px; top: 0; background-image: url('img/black1x1.png');">
+    <div style="float: left; flex: 1; height: 400px;overflow: hidden;">
+      <h1>Welcome to Hack Manhattan!</h1>           <!-- welcome -->
+      <div id="event-schedule"></div> <!-- events -->
 
 
-    <div style="float: left; width: 60%; margin-top:0">
-
-    <h1>Welcome to Hack Manhattan!</h1>
-    <div id="event-schedule"></div>
-
+      <div class="slackhead" style="position: absolute; bottom: 0; margin: 0; z-index:5">
+            Meanwhile, on the hackmanhattan.slack.com
+           <img src="img/slack.png" height="20" style="vertical-align: middle">hackerspace channel...</div>
     </div>
 
-    <center>
-    <div style="float: left; width:60%;">
-    <em style="font-size: 120%;">Dashboard under construction.
-                                Expect components and layout to change.</em>
-    </center>
+    <div style="float: right; width: 448px;"> <!-- right column -->
+      <div class="container">
+        <!-- doorcam: 640 x 480 -->
+        <img id="doorcam" width="448" height="336"
+             src="http://hackmanhattan.duckdns.org:8888/hm/dash/util/camera.php?view=door">
+        <div class="bottom-right" style="not-bottom:55">
+          <span id="clock"></span>
+        </div>
 
-    </div>
-
-
-
-  <div class="right" style="float: right; width: 448px;">
-    <div class="container" style="z-index: 5; position: absolute; top: 0;">
-      <!-- doorcam: 640 x 480 -->
-      <img id="doorcam" width="448" height="336"
-           src="http://hackmanhattan.duckdns.org:8888/hm/dash/util/camera.php?view=door">
-      <div class="bottom-right" style="bottom:55">
-        <span id="clock"></span>
+        <div class="top-right"><span id="weather"></span></div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js"></script>
+        <script src="js/weather.js"></script>
       </div>
 
-      <div class="top-right""><span id="weather"></span></div>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js"></script>
-      <script src="js/weather.js"></script>
-      <div class="slackhead" style="z-index:5">
-         Recently on the
-         <img src="img/slack.png" height="20"
-                  style="vertical-align:middle">hackerspace channel...</div>
     </div>
-
   </div>
 
+  <div style="position: absolute; bottom: 0; width: 100%;">
 
 
+
+       <div style="word-wrap: break-word;"
+             id="slack-chat"></div>
+           </div>
 </body>
 </html>
