0a8c69310612bd0ea5e7c69af9fa04566c4fc338
beadsland <beads.d.land@gmail.com>
2018-09-15 07:30:20 -0400

    celcius and weather emoji

--- html/js/weather.js
+++ html/js/weather.js
@@ -3,7 +3,16 @@
 $(document).ready(function() {
   getWeather(); //Get the initial weather.
   setInterval(getWeather, 600000); //Update the weather every 10 minutes.
+  setInterval(toggleTemp, 5000);
 });
+
+function toggleTemp() {
+  if ( $("#weather_temp").html() === localStorage.getItem("tempF") ) {
+    $("#weather_temp").html(localStorage.getItem("tempC"))
+  } else {
+    $("#weather_temp").html(localStorage.getItem("tempF"))
+  }
+}
 
 function getWeather() {
   $.simpleWeather({
@@ -11,10 +20,15 @@ function getWeather() {
     woeid: '',
     unit: 'f',
     success: function(weather) {
-      html = '<h2>&thinsp;<i class="icon-'+weather.code+'"></i>';
-      html += weather.temp+'&deg;F '+weather.humidity + '%';
+      localStorage.tempF = weather.temp + '<span class="degrees">℉</span>';
+      localStorage.tempC = weather.alt.temp + '<span class="degrees">℃</span>';
+
+      html = '&thinsp;<span class="emoji" id="moon-'+weather.code+'"></span>';
+      html += '<span class="emoji" id="icon-'+weather.code+'"></span>';
+      html += '<span id="weather_temp">' + localStorage.tempF + '</span> '
+      html += weather.humidity + '<span class="degrees">%</span>';
       html += '<span class="emoji">💦</span>';
-      html += '&thinsp;</h2>';
+      html += '&thinsp;';
 //      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
 //      html += '<li class="currently">'+weather.currently+'</li>';
 //      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
