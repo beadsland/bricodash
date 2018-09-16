f1e52d961c2b7c2c2cee24f068bc1393728292a3
beadsland <beads.d.land@gmail.com>
2018-09-15 22:01:36 -0400

    refactor HTML and experimenting with hide/show

--- html/js/weather.js
+++ html/js/weather.js
@@ -14,26 +14,46 @@ function toggleTemp() {
   }
 }
 
+function newtoggleTemp() {
+  console.log("toggle");
+  if ( $("#weather_ftemp").is(":visible") ) {
+    $("#weather_ftemp").hide();
+    $("#weather_ctemp").show();
+    console.log("yes")
+  } else {
+    $("#weather_ctemp").hide();
+    $("#weather_ftemp").show();
+    console.log("no")
+  }
+}
+
+function span(str, cls, id) {
+  if (id !== undefined) {
+    return '<span class="' + cls + '" id="' + id + '">' + str + '</span>'
+  } else {
+    return '<span class="' + cls + '">' + str + '</span>'
+  }
+}
+
 function getWeather() {
   $.simpleWeather({
     location: '10011',
     woeid: '',
     unit: 'f',
     success: function(weather) {
-      localStorage.tempF = weather.temp + '<span class="degrees">℉</span>';
-      localStorage.tempC = weather.alt.temp + '<span class="degrees">℃</span>';
+      localStorage.tempF = weather.temp + span("℉", "degrees");
+      localStorage.tempC = weather.alt.temp + span("℃", "degrees");
 
-      html = '&thinsp;<span class="emoji" id="moon-'+weather.code+'"></span>';
-      html += '<span class="emoji" id="icon-'+weather.code+'"></span>';
-      html += '<span id="weather_temp">' + localStorage.tempF + '</span> '
-      html += weather.humidity + '<span class="degrees">%</span>';
-      html += '<span class="emoji">💦</span>';
-      html += '&thinsp;';
-//      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-//      html += '<li class="currently">'+weather.currently+'</li>';
-//      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+      localStorage.humid = weather.humidity
+                          + span("%", "degrees") + span("💦", "emoji");
 
-      $("#weather").html(html);
+      html  = span("", "emoji", "moon-" + weather.code);
+      html += span("", "emoji", "icon-" + weather.code);
+      html += span(localStorage.tempF, "", "weather_temp")
+      html += " " + localStorage.humid;
+
+      $("#weather").html('&thinsp;' + html + '&thinsp;');
+      toggleTemp()
     },
     error: function(error) {
       $("#weather").html('<p>'+error+'</p>');
