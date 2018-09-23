f9d8806df76c50a1473f935f2e494eb6589ae25d
beadsland <beads.d.land@gmail.com>
2018-09-22 21:24:45 -0400

    add github to combo edit log

--- jobs/wiki.py
+++ jobs/wiki.py
@@ -6,7 +6,7 @@ import os
 import sys
 import time
 
-query = "https://wiki.hackmanhattan.com/api.php?action=query&list=recentchanges&rcprop=title%7Cids%7Csizes%7Cflags%7Cuser&rclimit=200&rcshow=!minor&format=json"
+query = "https://wiki.hackmanhattan.com/api.php?action=query&list=recentchanges&rcprop=title%7Cids%7Csizes%7Cflags%7Cuser%7Ctimestamp&rclimit=200&rcshow=!minor&format=json"
 response = requests.get(query)
 if response.status_code != 200:   sys.exit(response.status_code);
 result = json.loads(response.text)
@@ -14,6 +14,8 @@ result = json.loads(response.text)
 recent = result["query"]["recentchanges"]
 seen = []
 report = []
+wlogo = '<img class="logo" src="img/mediawiki.png">'
+glogo = '<img class="logo" src="img/github.png">'
 
 for edit in recent:
   if edit["title"] not in seen:
@@ -21,16 +23,32 @@ for edit in recent:
     for i in range(len(path)):
       if path[i].find(":") > -1:
         path[i] = '<span class="wiki-special">' + path[i] + "</span>"
-    report.append( "/&#x200a;".join(path) )
+    title = wlogo + "/&#x200a;".join(path)
+    html = '<div class="wiki-line">' + title + "</div>"
+    report.append( (edit["timestamp"], html) )
     seen.append( edit["title"] )
 
-report = list( ('<div class="wiki-line">' + e + "</div>" for e in report) )
-report = list( reversed(report[:15]) )
+query = "https://api.github.com/users/hackmanhattan/events"
+response = requests.get(query)
+if response.status_code != 200:   sys.exit(response.status_code);
+result = json.loads(response.text)
+
+for push in result:
+  if push["repo"]["name"] not in seen:
+    repo = push["repo"]["name"].replace("hackmanhattan/", "")
+    title = glogo + repo
+    html = '<div class="wiki-line">' + title + "</div>"
+    report.append( (push["created_at"], html) )
+    seen.append( push["repo"]["name"] )
+
+report = sorted(report)
+report = list(s for (t,s) in report)
+
 
 report.append( '<span id="timestamp" epoch="' + str(time.time()) + '"></span>' )
 
 pwd = os.path.dirname(sys.argv[0])
 filename = pwd + "/../html/pull/wiki.html"
 file = open(filename + ".new", "w")
-file.write( u"\n".join(report) )
+file.write( u"\n".join(report[15:]) )
 os.rename(filename + ".new", filename)
