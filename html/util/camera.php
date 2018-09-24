03d3bb65a1b7202f9c7e16733917f2fb57d86e15
beadsland <beads.d.land@gmail.com>
2018-09-23 22:20:06 -0400

    doc credit

--- html/util/camera.php
+++ html/util/camera.php
@@ -23,6 +23,11 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. */
 
+/*
+ Multiple feeds and snapshot functionality added by Beads Land-Trujillo
+ at Hack Manhattan, 2018
+ */
+
 // config:
 $mjpg = array(
         'space' => "http://192.168.43.125:8080/",
