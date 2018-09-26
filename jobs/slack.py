d82c0381c815563595420b29ba574db1340cd0a0
beadsland <beads.d.land@gmail.com>
2018-09-26 02:56:32 -0400

    embed image thumbnails in slack

--- jobs/slack.py
+++ jobs/slack.py
@@ -8,6 +8,7 @@ import emoji_data_python
 import time
 import humanize
 import datetime
+import requests
 
 pwd = os.path.dirname(sys.argv[0])
 token_file = ".keys/slacker_token"
@@ -37,18 +38,50 @@ def hid(s): return "<span style='opacity: .25;'>" + s + "</span>"
 def whn(s): return "<span class='slacked'>" + s + "</span>"
 def usp(s): return "<span class='slacker'>@" + s + "</span>"
 def who(s): return "<span class='slackee'>" + s + "</span>"
-def avt(s): return "<img class='logo' src=\"" + s + "\">"
+def avt(s): return "<img class='logo' src='" + s + "' alt='" + s + "'>"
 
 emjex = re.compile(r":([A-Za-z\-_]+):")
 usrex = re.compile(r"<@([^\|>]+)\|([^>]+)>")
 us2ex = re.compile(r"<@([^\|>]+)>")
 chnex = re.compile(r"<#[A-Z0-9]+\|([^>]+)>")
 lnkex = re.compile(r"<(http.*)>")
-imgext = ('.gif', '.jpg', '.jpeg', '.png',
-          '.GIF', '.JPG', '.JPEG', '.PNG')
+
+def fetch_image(file, url, token):
+  headers = { "Authorization": "Bearer " + token }
+  response = requests.get(url, headers=headers, stream=True)
+  if response.status_code != 200:   sys.exit(response.status_code);
+  with open(file, 'wb') as handle:
+    for block in response.iter_content(1024):
+      handle.write(block)
+
+imgext = ('.gif', '.jpg', '.jpeg', '.png')
+
+def intlnk(u):
+  file = time.strftime("%Y%m%d-%H%M%S_") + u.split("?")[0].split("/")[-1]
+  thmb = os.path.join(pwd, "../html/thmb/")
+  fetch_image(thmb + file, u, token)
+
+  for f in os.listdir(thmb):
+    f = os.path.join(thmb, f)
+    if os.stat(f).st_mtime < time.time() - 20 * 60:
+      if os.path.isfile(f):
+        os.remove(f)
+
+  return ' ' + avt("thmb/" + file)
+
 def extlnk(o, u, c):
-  if u.endswith(imgext) and o == "<":     return ' ' + avt(url)
-  else:                                   return ' ' + o + u + c
+  if u.split("?")[0].lower().endswith(imgext) and o == "&lt;":
+    return ' ' + avt(u)
+  else:
+    if u.split("?")[0].lower().endswith(imgext) and o == "[":
+      return intlnk(u)
+    else:
+      if len(u) < 50:
+        start = len(u) - 20
+      else:
+        start = 30
+      return ' ' + o + u[:start] + '…' + u[-20:] + c
+
 
 semoji = slack.emoji.list().body["emoji"];
 def emjlnk(name):
@@ -83,11 +116,12 @@ for message in reversed(response.body['messages']):
 
   if 'files' in message:
     for file in message['files']:
-      url = file['thumb_64']
-      text += ' ' + extlnk("[", url[:30], "…]")
-      print(message)
+      if 'thumb_64' in file:
+        text += ' ' + extlnk("[", file['thumb_64'], "]")
+      else:
+        text += ' ' + extlnk("[", file['permalink'], "]")
 
-  text = lnkex.sub(lambda m: extlnk("&lt;", m.group(1)[:30], "…&gt;"), text)
+  text = lnkex.sub(lambda m: extlnk("&lt;", m.group(1), "&gt;"), text)
   text = chnex.sub(lambda m: "#" + m.group(1), text)
   text = usrex.sub(lambda m: usp(names.get(m.group(1), m.group(2))), text)
   text = us2ex.sub(lambda m: usp(names.get(m.group(1), m.group())), text)
