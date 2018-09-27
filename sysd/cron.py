03a4877b933186c0f11d30b924f47488773e50e8
beadsland <beads.d.land@gmail.com>
2018-09-27 10:02:23 -0400

    no delays

--- sysd/cron.py
+++ sysd/cron.py
@@ -18,14 +18,14 @@ min = datetime.datetime.now().minute
 hr = datetime.datetime.now().hour
 pwd = os.path.dirname(sys.argv[0])
 
-if 1:                os.system(pwd + "/../jobs/slack.py")
-if 1:                os.system(pwd + "/../jobs/mta.py")
-if min % 5 == 0:     os.system(pwd + "/../jobs/photo.py")
-if min % 60 == 0:    os.system(pwd + "/../jobs/wiki.py")
+if 1:                os.system(pwd + "/../jobs/slack.py &")
+if 1:                os.system(pwd + "/../jobs/mta.py &")
+if min % 5 == 0:     os.system(pwd + "/../jobs/photo.py &")
+if min % 60 == 0:    os.system(pwd + "/../jobs/wiki.py &")
 
-if min == 0 and hr == 0: os.systemd(pwd + "/../jobs/cal.py")
-if min % 60 == 0:    os.system(pwd + "/../jobs/brite.py")
-if min % 30 == 5:    os.system(pwd + "/../jobs/upmeet.py")
-if min % 10 == 0:    os.system(pwd + "/../jobs/events.py")
+if min == 0 and hr == 0: os.systemd(pwd + "/../jobs/cal.py &")
+if min % 60 == 0:    os.system(pwd + "/../jobs/brite.py &")
+if min % 30 == 5:    os.system(pwd + "/../jobs/upmeet.py &")
+if min % 10 == 0:    os.system(pwd + "/../jobs/events.py &")
 
 if 1:                os.system(pwd + "/../jobs/sous/veil.py &")
