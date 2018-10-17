c5e8ca59575fe0777579ae1b401362f744148a5d
beadsland <beads.d.land@gmail.com>
2018-10-17 14:00:38 -0400

    refactor holidays

--- jobs/brico/events/holiday.py
+++ jobs/brico/events/holiday.py
@@ -15,12 +15,15 @@
 ## along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ####
 
+import dateutil.parser
+from dateutil.relativedelta import relativedelta
 import datetime
 import subprocess
 import json
 import os
 import sys
 
+import brico.common
 import brico.common.html as html
 
 def main():
@@ -29,32 +32,57 @@ def main():
   usnat = "cal/usnat.cal"
   local = "cal/local.cal"
 
-  pwd = os.path.dirname(sys.argv[0])
+  arr = sorted( parse_cal(birth, 1) + parse_cal(geek, 4) \
+                + parse_cal(usnat, 7) + parse_cal(local) )
+  arr = ( { 'start': t[0].decode('utf-8'),
+            'venue': "Holiday",
+            'event': t[1].encode().decode('utf-8') } for t in arr )
+  arr = list(arr)
 
-  def recur(str): return subprocess.check_output([pwd + "/brico/events/recur.pl", str])
+  # Three events minimum.
+  n = 3
+  # Grab all events for today...
+  tomorrow = datetime.datetime.now() + relativedelta(days = +1)
+  while n < len(arr) and \
+                         dateutil.parser.parse(arr[n]['start']) < tomorrow:
+    n += 1
+  # ...and up to max 6 total events through tomorrow.
+  nextday = tomorrow + relativedelta(days = +1)
+  while n < len(arr) < 6 and \
+                         dateutil.parser.parse(arr[n]['start']) < nextday:
+    n += 1
 
-  def parse_cal(filename):
-    arr = []
-    with open(pwd + "/" + filename, "rb") as f:
-      for line in f.readlines():
-        if line.strip() != "":
-          spl = line.strip().decode().split("::")
-          if len(spl) > 2 and spl[2].strip() != "":
-            full = "%s %s" % (spl[1], html.emoji(spl[2].strip()))
-            arr.append( (spl[0], full) )
-          elif len(spl) > 1:
-            arr.append( (spl[0], spl[1]) )
+  brico.common.write_json("holiday.json", arr[:n])
 
-    arr = (( recur(t[0]), t[1].strip() ) for t in arr)
-    arr = sorted(arr)
-    return arr
+###
+# Process a human-readable calendar file
+###
+def parse_cal(filename, days=30):
+  arr = []
 
-  arr = sorted( parse_cal(geek)[:3] + parse_cal(birth)[:3] + parse_cal(usnat)[:3] + parse_cal(local)[:3] )
-  arr = ( { "start": t[0].decode('utf-8'), "venue": "Holiday", "event": t[1].encode().decode('utf-8') } for t in arr )
+  with open(os.path.join(brico.common.pwd(), filename), "rb") as f:
+    for line in f.readlines():
+      if line.strip() != "" and not line.startswith(b'#'):
+        spl = line.strip().decode().split("::")
+        if len(spl) > 2 and spl[2].strip() != "":
+          spl[2] = spl[2].strip()
+          moji = html.emoji(spl[2]) if len(spl[2]) < 5 else html.logo(spl[2])
+          arr.append( (spl[0], "%s %s" % (spl[1], moji)) )
+        elif len(spl) > 1:
+          arr.append( (spl[0], spl[1]) )
 
-  holidays = list(arr)
+  arr = (( recur(t[0]), t[1].strip() ) for t in arr)
+  arr = sorted(arr)
 
-  filename = pwd + "/../html/pull/holiday.json"
-  file = open(filename + ".new", "w")
-  file.write( json.dumps(holidays[:4]) )
-  os.rename(filename + ".new", filename)
+  n = 0
+  cap = datetime.datetime.now() + relativedelta(days = +days)
+  while dateutil.parser.parse(arr[n][0]) < cap: n += 1
+
+  return arr[:n]
+
+###
+# Haven't found a python module that does this for all our use cases
+###
+def recur(str):
+  recur = os.path.join(brico.common.pwd(), "brico/events/recur.pl")
+  return subprocess.check_output([recur, str])
