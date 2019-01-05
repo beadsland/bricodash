8fc3b63d1f9354ae24c6dd373a916bd9a11561a9
beadsland <beads.d.land@gmail.com>
2019-01-05 14:17:53 -0500

    minor tweak to order of operations

--- jobs/events.py
+++ jobs/events.py
@@ -60,8 +60,8 @@ if min % 60 == 0 or 'brite' in sys.argv:      brico.events.brite.main()
 if min % 30 == 0 or 'upmeet' in sys.argv:     brico.events.upmeet.main()
 if min % 60 == 0 or 'tober' in sys.argv:      brico.events.tober.main()
 if min % 60 == 0 or 'castles' in sys.argv:    brico.events.castles.main()
-if min % 10 == 0 or 'space' in sys.argv:      brico.events.space.main()
 if min % 60 == 0 or 'private' in sys.argv:    brico.events.private.main()
+if min % 10 == 0 or 'space' in sys.argv:      brico.events.space.main()
 
 ###
 # Build events component divs
