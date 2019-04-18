17fd437cf52bd257da9f34d2f269b987a4b1062b
beadsland <beads.d.land@gmail.com>
2019-04-18 08:17:45 -0400

    refactor in prep for troubleshooting Chromecast API changes

---
+++ sysd/app/__init__.py
@@ -0,0 +1,56 @@
+"""
+Entry point for loop to run DashCast semi-persistently on a Chromecast while
+allowing other Chromecast apps to work also by only launching when idle.
+
+Based on https://github.com/madmod/dashcast-docker
+
+Adapted for hackmanhattan/defaultcast by @mz@hackmanhattan.slack.com, 2017.
+"""
+
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.propagate = False
+
+import pychromecast
+import time
+
+import app.launch
+
+def main(DISPLAY_NAME, DASHBOARD_URL, IGNORE_CEC):
+    logger.debug('Hello defaultcast')
+
+    if IGNORE_CEC:
+        logger.debug('Ignoring CEC for Chromecast ' + DISPLAY_NAME)
+        pychromecast.IGNORE_CEC.append(DISPLAY_NAME)
+
+    casts = []
+    attempt = 0
+
+    while attempt < 5 and len(casts) == 0:
+        logger.debug('Searching for Chromecasts...')
+        casts = pychromecast.get_chromecasts()
+
+        if len(casts) == 0 and attempt == 4:
+            logger.debug('No Devices Found')
+            exit()
+
+        if len(casts) == 0:
+            attempt += 1
+            time.sleep(30)
+
+    cast = next(cc for cc in casts if DISPLAY_NAME in (None, '') \
+                                      or cc.device.friendly_name == DISPLAY_NAME)
+
+    if not cast:
+        logger.debug('Chromecast with name ' + DISPLAY_NAME + ' not found')
+        exit()
+
+    defaultcast = app.launch.DashboardLauncher(cast, dashboard_url=DASHBOARD_URL)
+
+    try:
+        while True:
+            defaultcast.check()
+    except Exception as e:
+        logger.debug(e)
+        exit()
---
+++ sysd/app/launch.py
@@ -0,0 +1,70 @@
+
+"""
+Launcher class to run DashCast semi-persistently on a Chromecast while
+allowing other Chromecast apps to work also by only launching when idle.
+
+Based on https://github.com/madmod/dashcast-docker
+
+Adapted for hackmanhattan/defaultcast by @mz@hackmanhattan.slack.com, 2017.
+"""
+
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.propagate = False
+
+import pychromecast.controllers.dashcast as dashcast
+
+class DashboardLauncher():
+    def __init__(self, device, dashboard_url='https://home-assistant.io', dashboard_app_name='DashCast'):
+        self.device = device
+        logger.debug('DashboardLauncher ' + self.device.name)
+
+        self.controller = dashcast.DashCastController()
+        self.device.register_handler(self.controller)
+
+        receiver_controller = device.socket_client.receiver_controller
+        receiver_controller.register_status_listener(self)
+
+        self.dashboard_url = dashboard_url
+        self.dashboard_app_name = dashboard_app_name
+
+        self.launch_checked = 0
+
+    def check(self):
+        """ Called when a new cast status has been received."""
+        self.launch_checked += 1
+        logger.debug('Checking if we should launch. (i: {})'
+                     .format(self.launch_checked))
+        if self.device.status is not None:
+            logger.debug('Current app: {}'
+                         .format(repr(self.device.status.display_name)))
+
+        if self.should_launch():
+            logger.debug('I think we should launch. If this is the case in 15 seconds as well, we will launch.')
+            time.sleep(15)
+
+        # Launch also when we've been going for 12 hours continously and it's
+        # 3am (no matter if a different app is active or it's our cast)
+        if self.should_launch():
+            self.launch_dashboard()
+
+        time.sleep(15)
+
+    def should_launch(self):
+        """ If the device is active, the dashboard is not already active, and no other app is active."""
+        return ((self.device.status is not None and
+                 self.device.status.display_name in ('Backdrop',)) or
+                (self.launch_checked > 2880 and
+                 time.localtime().tm_hour == 3 and
+                 self.device.status is not None and
+                 self.device.status.display_name in ('DashCast',)))
+
+    def launch_dashboard(self):
+        logger.debug('Launching dashboard on Chromecast ' + self.device.name)
+        try:
+            self.controller.load_url(self.dashboard_url)
+            self.launch_checked = 0
+        except Exception as e:
+            logger.debug(e)
+            pass
