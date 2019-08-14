ef285323e73607ec0726908f957a7d5276a75c4f
beadsland <beads.d.land@gmail.com>
2019-08-14 16:16:17 -0400

    declutter observer

--- sysd/bindsight/config/dev.exs
+++ sysd/bindsight/config/dev.exs
@@ -4,9 +4,7 @@ config :bindsight,
   cameras: %{
     test: "http://rfid-access-building.lan:8080/",
     space: "http://wrtnode-webcam.lan:8080/",
-    door: "http://rfid-access-building.lan:8080/",
-    cr10: "http://octoprint-main.lan:8080/",
-    hydro: "http://hydrocontroller.lan:8081/"
+    door: "http://rfid-access-building.lan:8080/"
   },
   cowboy_acceptors: 5,
   cluck_errors: true,
