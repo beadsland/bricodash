d95cccd13751fb0120bd12a413217c81343bf092
beadsland <beads.d.land@gmail.com>
2019-10-24 06:03:38 -0400

    document state of library and hardcode 2019 dates

--- jobs/brico/events/lunar/hindu.py
+++ jobs/brico/events/lunar/hindu.py
@@ -56,50 +56,53 @@ def diwali(now):
   newmoon = ephem.next_new_moon(navaratri).datetime().replace(tzinfo=pytz.UTC)
 
   hindu = lunar_date(newmoon)
-
-  # Examine whether library actually calculates tithi and whether they can be extracted.
-
-  # Should calculate all dates based on India Standard Timezone
-
-  # "A particular day is ruled by the tithi at Sunrise"
-
-  # Calendar day of a tithi is deemed to be calendar day of start of tithi
-
-
-  # Library appears to be reporting gregorian day on which lunar tithi ends,
-  # thus we use prior tithi to calculate gregorian day on which next tithi begins.
-
-  # 2016 Deepawali started two days earlier???
-  # 2018 Deepawali started one day earlier???
-
-  # 2021 Deepawali starts one day earlier, we calculate Diwali for same day but ending day earlier also???
-
-  # If these calculations are correct, Diwali in 2028 ought to be only 4 days.
-
-  ##
-  # Solar day is determined by last muharat (sunset period) during the tithi...
-  #
-  # 2016: Dwadashi tithi Oct 26 2:30 to Oct 27 4:14 <-- muharat on 26th
-  #     : Dhantaras ends Oct 28 6:21 <-- second of two muharat, so 28th is solar day
-  #
-  ##
-
   dwadashi = gregorian((hindu[0], hindu[1], 26, hindu[3]))
   first = gregorian((hindu[0], hindu[1], 27, hindu[3]))
   pratipad = next_pratipad(lunar_date(newmoon))
   last = gregorian((pratipad[0], pratipad[1], 1, pratipad[3]))
+
+  # Library appears to be reporting gregorian day on which lunar tithi ends,
+  # thus we use prior tithi to calculate gregorian day on which next tithi begins.
 
   if sunrise(newmoon.date()) > newmoon:
     diwali = newmoon.date() - datetime.timedelta(days=1)
   else:
     diwali = newmoon.date()
 
-#  print(dwadashi, first, diwali, last)
+  # All the above is near-useless. The best available F/OSS Hindu calendar
+  # library, written in Lisp, only provides for civil, or solar, days (divasa).
+  # It does not expose lunar days (tithi). As lunar days vary in length, it is
+  # not uncommon for an entire lunar day to begin and end in the course of a
+  # solar day, or vice versa.
+  #
+  # Many Hindu holidays, including Diwali, occur in accordance with the lunar
+  # calendar, but are celebrated following the civil calendar. However, which
+  # lunar day maps back to any given solar day varies depending on context.
+  # Usually, the solar day of a tithi is determined by sunrise. Yet for festivals
+  # with evening celebrations, such as diwali, it is determined by sunset.
+  # Where more than one sunset (or sunrise) occurs in the same tithi, the practice
+  # appears to be to recognize the tithi on the later solar day.
+  #
+  # Our currently library not only fails to expose tithi, it also works with only
+  # rough estimate of sunrise, and takes no account of geographic location. This
+  # together with regional differences on the significance of historical events
+  # leading up to the festival of lights, means that we really need to accurately
+  # determine tithi start and end times to accurately identify local start and
+  # end dates, and clearly indicate local vs. various national variants.
+  #
+  # Reference: http://www.astrojyoti.com/understanding-panchangam.htm
+  # Calculation Validation: https://www.drikpanchang.com/diwali/diwali-puja-calendar.html
 
-  emoji = brico.common.html.logo("img/ggl-diwa.png")
+  emoji = brico.common.html.logo("img/ggl-diwa.png", ":diya:")
+  vary = brico.common.html.em("(%s)" % "varies")
+
+  # We're hard-coding for now because we aren't calculating tithi correctly
+  first = datetime.date(2019, 10, 25)
+  diwali = datetime.date(2019, 10, 27)
+  last = datetime.date(2019, 10, 29)
 
   if now < sunrise(first):
-    (event, morn) = ("Diwali Begins %s" % emoji, sunrise(first))
+    (event, morn) = ("Diwali Begins %s %s" % (vary, emoji), sunrise(first))
   elif now.date() == first:
     (event, morn) = ("Happy Diwali! %s" % emoji, sunrise(first))
   elif now < sunrise(diwali):
@@ -116,7 +119,7 @@ def diwali(now):
   else:
     return [{'start': iso(morn), 'venue': "Holiday", 'event': event},
             {'start': iso(sunrise(last + datetime.timedelta(days=1))),
-             'venue': "Holiday", 'event': "Diwali Ends %s" % emoji}]
+             'venue': "Holiday", 'event': "Diwali Ends %s %s" % (vary, emoji)}]
 
 def next_happens(today, lunar):
   if lisp.function('old-hindu-lunar-precedes')(today, lunar):
